8cb2dd6366a2aa6597d1064711e1e42ef7bd00fd
mDennis89 <mdemmis@my.centennialcollege.ca>
2023-06-24 23:16:13 -0400

    commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
    // URI: "mongodb://127.0.0.1:27017/contacts",
-    URI: "mongodb+srv://melissadennis:ZQwUGfL3NJFrhLyX@cluster1.ndlvuzw.mongodb.net/?retryWrites=true&w=majority"
+    URI: "mongodb+srv://melissadennis:ZQwUGfL3NJFrhLyX@cluster1.ndlvuzw.mongodb.net/"
   };
   
