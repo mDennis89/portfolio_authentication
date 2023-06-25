488ad231ad822616bc337a11ba2edcce9192a410
mDennis89 <mdemmis@my.centennialcollege.ca>
2023-06-24 22:30:05 -0400

    --changes in db connection --changes int db connection --changes in db connection --changes in db connection

--- server/config/app.js
+++ server/config/app.js
@@ -90,4 +90,6 @@ app.use(function(err, req, res, next) {
   res.render('error', {title: 'Error'});
 });
 
+
+
 module.exports = app;
--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
-module.exports = 
-{
-    "URI": "mongodb://127.0.0.1:27017/contacts"
-}
+module.exports = {
+   // URI: "mongodb://127.0.0.1:27017/contacts",
+    URI: "mongodb+srv://melissadennis:ZQwUGfL3NJFrhLyX@cluster1.ndlvuzw.mongodb.net/?retryWrites=true&w=majority"
+  };
+  
