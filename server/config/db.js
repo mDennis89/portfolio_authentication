0b5b901aa0dcbcc92316f5de49719a2311cb281e
mDennis89 <mdemmis@my.centennialcollege.ca>
2023-07-18 15:24:43 -0400

    updated files

--- server/config/db.js
+++ server/config/db.js
@@ -4,5 +4,3 @@ module.exports =
     URI: "mongodb+srv://melissadennis:5SqgRqWQ7R0mkFoO@cluster1.ndlvuzw.mongodb.net/Contact?retryWrites=true&w=majority",
    "Secret": 'SomeSecret'
 }
-  
-  
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,4 @@
-
-
-module.exports = router;var express = require('express');
+var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
