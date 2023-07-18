b18844905a86cdad50cc194f91754ba76e88b92e
mDennis89 <mdemmis@my.centennialcollege.ca>
2023-07-17 23:30:46 -0400

    updated db file

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 module.exports = {
    //URI: "mongodb://127.0.0.1:27017/contacts",
    
-   URI: "mongodb+srv://melissadennis:RUBaA7K98PeveLXy@cluster1.ndlvuzw.mongodb.net/Contact?retryWrites=true&w=majority"
+   URI: "mongodb+srv://melissadennis:5SqgRqWQ7R0mkFoO@cluster1.ndlvuzw.mongodb.net/Contact?retryWrites=true&w=majority"
   };
   
   
